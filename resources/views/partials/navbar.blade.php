de67681a57953809427269ba941de32d9b359018
Gwyn Jones <gwyn.jones@nationalarchives.gsi.gov.uk>
2017-01-10 19:05:06 +0000

    Add demonstration of notification center

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -12,12 +12,26 @@
         </div>
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav navbar-right">
-                <li><a href="#"><span class="glyphicon glyphicon-envelope message"></span></a></li>
+                <li><a href="#" data-toggle="modal" data-target=".bs-example-modal-sm"><span
+                                class="glyphicon glyphicon-envelope message"></span></a></li>
                 <li><a href="#"><span class="glyphicon glyphicon-bell"></span></a></li>
                 <li><a href="#"><span class="glyphicon glyphicon-user"></span></a></li>
                 @include('partials.authentication-links')
             </ul>
         </div>
-
     </div>
 </nav>
+
+<div class="modal fade bs-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel">
+    <div class="modal-dialog modal-sm" role="document">
+        <div class="panel panel-success">
+            <div class="panel-heading">Batch LEV2359B008 receipt confirmed</div>
+            <div class="panel-body">
+                <p>The National Archives confirmed receipt of hard disk containing Batch LEV2359B008 on 14 February
+                    2017</p>
+                <a href="#" class="btn btn-sm btn-primary pull-right">View batch</a>
+            </div>
+        </div>
+
+    </div>
+</div>
