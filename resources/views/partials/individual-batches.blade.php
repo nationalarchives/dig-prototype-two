33c0018a0720e99f5970e7843bb8219ec6cf057a
Punal Chotrani <punalsc@gmail.com>
2017-02-03 12:00:30 +0000

    changed metaStatus to transferStatus.

--- resources/views/partials/individual-batches.blade.php
+++ resources/views/partials/individual-batches.blade.php
@@ -1,3 +1,3 @@
 <li class="list-group-item">
-    <a href="/batch/{{$batch->id}}">{{ $batch->name }}</a> <span class="label label-{{ ($batch->metadataHasBeenUploaded()) ? 'success' : 'default' }} pull-right">{{ $batch->metadataStatus() }}</span>
+    <a href="/batch/{{$batch->id}}">{{ $batch->name }}</a> <span class="label label-{{ ($batch->metadataHasBeenUploaded()) ? 'success' : 'default' }} pull-right">{{ $batch->transferStatus() }}</span>
 </li>
