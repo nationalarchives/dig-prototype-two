9ea571ea24285452d66a8788e4c8d0583ca53b82
Punal Chotrani <punalsc@gmail.com>
2017-02-03 12:35:58 +0000

    minor fixes to the batches url.

--- resources/views/partials/individual-batches.blade.php
+++ resources/views/partials/individual-batches.blade.php
@@ -1,3 +1,3 @@
 <li class="list-group-item">
-    <a href="/batch/{{$batch->id}}">{{ $batch->name }}</a> <span class="label label-{{ ($batch->metadataHasBeenUploaded()) ? 'success' : 'default' }} pull-right">{{ $batch->transferStatus() }}</span>
+    <a href="/batches/{{$batch->id}}">{{ $batch->name }}</a> <span class="label label-{{ ($batch->metadataHasBeenUploaded()) ? 'success' : 'default' }} pull-right">{{ $batch->transferStatus() }}</span>
 </li>
