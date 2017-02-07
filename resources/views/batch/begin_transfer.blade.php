941d98bbdfee3a3421c55850c0c0351fd8d2ecf3
Punal Chotrani <punalsc@gmail.com>
2017-02-07 17:47:19 +0000

    Added success messages for uploaded metadata.

--- resources/views/batch/begin_transfer.blade.php
+++ resources/views/batch/begin_transfer.blade.php
@@ -11,39 +11,32 @@
 
 @section('content')
     <h1>{{ trans('editorial.static_pages.begin_transfer.heading') }}</h1>
-    {{--<form class="dropzone" action="{{ route('batches.transfer_files', ['id' => $batch->id]) }}" method="post"--}}
-          {{--enctype="multipart/form-data" multiple="multiple">--}}
-        {{--{{ csrf_field() }}--}}
-        {{--<div class="form-group">--}}
-            {{--<input type="hidden" name="id" value="{{ $batch->id }}">--}}
-            {{--<input type="hidden" name="_method" value="PATCH">--}}
-            {{--<label for="reference">{{ trans('editorial.batches.upload.label') }}</label>--}}
-            {{--<input type="file" name="transfer_file">--}}
-        {{--</div>--}}
-        {{--<div class="form-group">--}}
-            {{--<input type="submit" class="btn btn-default">--}}
-        {{--</div>--}}
-    {{--</form>--}}
-    <form action="{{ route('batches.transfer_files', ['id' => $batch->id]) }}" id="dragDrop" method="post" enctype="multipart/form-data">
+    <form class="dragDrop" action="{{ route('batches.transfer_files', ['id' => $batch->id]) }}" method="post" enctype="multipart/form-data">
         {{ csrf_field() }}
         <div class="form-group file-area">
-            <input type="file" name="files" id="images" required="required" multiple="multiple"/>
+            <input type="hidden" name="id" value="{{ $batch->id }}">
+            <input type="hidden" name="_method" value="PATCH">
+            <input type="file" name="transfer_file" required="required" multiple="multiple"/>
             <div class="file-dummy">
-                <div class="success">Great, your file(s) are selected and ready to be uploaded.</div>
                 <div class="default">
-                    <span class="glyphicon glyphicon-upload" aria-hidden="true"></span>
-                    <p>Drag your files here or click in this area</p>
+                    <h4>Drag your files here or click in this area</h4>
+                </div>
+                <div class="success">
+                    <h4>Great, your file(s) are selected and ready to be uploaded.</h4>
+                    <hr/>
+                </div>
+                <div class="success">
+                    <h5>File list</h5>
+                    <ul class="list-group text-left">
+                        <li class="list-group-item">metadata.csv <span class="glyphicon glyphicon-ok" aria-hidden="true"></span></li>
+                        <li class="list-group-item">metadata2.csv <span class="glyphicon glyphicon-ok" aria-hidden="true"></span></li>
+                        <li class="list-group-item">metadata3.csv <span class="glyphicon glyphicon-ok" aria-hidden="true"></span></li>
+                    </ul>
                 </div>
             </div>
         </div>
-
         <div class="form-group">
-            <button class="uploadButton" type="submit">Upload file(s)</button>
+            <input class="uploadButton" type="submit" value="Upload file(s)">
         </div>
     </form>
-    <div id="files">
-        <h4>File list</h4>
-        <ul id="filePreview"></ul>
-        <a id="remove" href="#" title="Remove file">Remove file</a>
-    </div>
 @endsection
