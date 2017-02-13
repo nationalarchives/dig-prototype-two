e3c5365a6a488b569f4a19ab3f7882244570ddec
Gwyn Jones <m@gwnjns.com>
2017-02-13 13:53:20 +0000

    Update application content for user testing
    Content updates received from team. Integrated for today's user
    testing.

--- database/seeds/BatchesTableSeeder.php
+++ database/seeds/BatchesTableSeeder.php
@@ -13,12 +13,20 @@ class BatchesTableSeeder extends Seeder
     public function run()
     {
         foreach (\App\Series::all() as $series) {
-            $batch = factory(App\Batch::class, rand(1, 2))->create([
+            factory(App\Batch::class)->create([
                 'series_id' => $series->id,
-                'name' => $series->reference . 'Y17B00',
-                'path_to_schema_file' => $series->reference . 'Y17B000' . '.csvs',
+                'name' => $series->reference . 'Y17B001',
+                'path_to_schema_file' => 'metadata_v9_' . $series->reference . 'Y17B0001' . '.csvs',
                 'path_to_metadata_file' => false,
             ]);
+
+            factory(App\Batch::class)->create([
+                'series_id' => $series->id,
+                'name' => $series->reference . 'Y17B002',
+                'path_to_schema_file' => 'metadata_v9_' . $series->reference . 'Y17B0002' . '.csvs',
+                'path_to_metadata_file' => false,
+            ]);
+
         }
     }
 }
--- database/seeds/SeriesTableSeeder.php
+++ database/seeds/SeriesTableSeeder.php
@@ -13,7 +13,13 @@ class SeriesTableSeeder extends Seeder
     public function run()
     {
         foreach (\App\Collection::all() as $collection) {
-            $series = factory(App\Series::class)->create(['collection_id' => $collection->id]);
+            factory(App\Series::class)->create([
+                'collection_id' => $collection->id,
+                'reference' => 'RW33',
+                'name' => 'Records of the Preservation and Digital Preservation Department',
+                'description' => '	
+The series contains digital records created in the Preservation area of The National Archives functional electronic file plan used between 2003 and 2008; the records would have been predominantly created by the various Preservation and Digital Preservation departments. This function concerns the devising and promulgating of pro-active and preventative strategies for the preservation of both government and non-public archival records. This involves managing the integrity and usability of all records, traditional and digital, in The National Archives. It includes developing internal and external preservation strategies and systems, researching physical and chemical properties of materials, and applying conservation techniques.'
+            ]);
         }
     }
 }
