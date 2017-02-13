8e717b696797d5af3ecee1466bfc28e66a915314
Gwyn Jones <m@gwnjns.com>
2017-02-13 17:27:24 +0000

    Introduce ability to add a series to a collection

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Series;
+use App\Collection;
 
 class SeriesController extends Controller
 {
@@ -15,5 +16,29 @@ class SeriesController extends Controller
     public function show(Series $series)
     {
         return view('series.show', compact('series'));
+    }
+
+    public function create(Collection $collection)
+    {
+        return view('series.create', compact('collection'));
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required',
+            'type' => 'required',
+            'description' => 'required',
+            'collection_id' => 'required',
+            'reference' => 'required'
+        ]);
+
+        $series = new Series($request->all());
+
+        $series->save();
+
+        flash(trans('editorial.series.store.success_message', ['name' => $series->name]), 'success');
+
+        return redirect()->route('series.index');
     }
 }
