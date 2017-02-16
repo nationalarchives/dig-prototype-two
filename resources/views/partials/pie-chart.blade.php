e3496e4a008c0c71709933631813a70bfff8cafd
Punal Chotrani <punalsc@gmail.com>
2017-02-16 15:34:17 +0000

    increased chart size.

--- resources/views/partials/pie-chart.blade.php
+++ resources/views/partials/pie-chart.blade.php
@@ -3,6 +3,6 @@
         <h3 class="panel-title">{{ trans('editorial.series.summary_heading') }}</h3>
     </div>
     <div class="panel-body">
-        <div id="piechart"></div>
+        <img class="img-responsive" src="http://chart.apis.google.com/chart?cht=p&chs=850x350&chdl=Open+records%7CClosed+records+with+closed+description%7CClosed+records+with+open+description&chl=Open+records%7CClosed+records+with+closed+description%7CClosed+records+with+open+description&chco=4286f4|f45641|41e2f4,4286f4|6699CC|CC33FF&chp=1.48350972222&chtt=Records+described+in+CSV+metadata&chts=000000,18&chd=t:50,10,5|50,25,25" />
     </div>
 </div>
